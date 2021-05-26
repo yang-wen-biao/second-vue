acc4fc7f82ec1a5cedece6e829cd36f1a273c3a4
yang-wen-biao <508213151@qq.com>
2021-05-26 09:55:01 +0800

    Default Changelist

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI, {Message} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import store from './store'
+import {findAllMenus} from "./config/menu";
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+Vue.use(ElementUI)
+
+router.beforeEach((to,from,next)=> {
+  if(to.path=='/home'){
+      if (window.sessionStorage.getItem("user")) {
+          let l=router;
+          findAllMenus(router,store)
+          if(l==router) console.log(router);
+          next();
+        } else {
+          next('/login');
+        }
+    next();
+  }
+  else next();
+})
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,146 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+import Login from '../views/Login'
+import firstHome from "@/views/FirstHome";
+import first from "@/views/FirstHome/First";
+import introduce from "@/views/FirstHome/introduce";
+
+import team from "@/views/FirstHome/team";
+import CourseDevelopment from "@/views/FirstHome/CourseDevelopment";
+import courseresource from "@/views/FirstHome/courseresource";
+import console from "@/views/FirstHome/console";
+import coursestudy from "@/views/FirstHome/coursestudy";
+
+import team1 from "@/views/FirstHome/Team/team1";
+import description from "@/views/FirstHome/course/description";
+import arrange from "@/views/FirstHome/course/arrange";
+import outline from "@/views/FirstHome/course/outline";
+import notion from "@/views/FirstHome/course/notion";
+import teacher1 from "@/views/FirstHome/TeacherResource/teacher1";
+
+Vue.use(VueRouter);
+
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    hidden: true
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path:'/',
+    name: 'FirstHome',
+    component: firstHome,
+    hidden: true,
+    children:[
+      {
+        path:'first',
+        name:'first',
+        component: first,
+        hidden:true,
+      },
+      {
+        path:'introduce',
+        name:'introduce',
+        component: introduce,
+        hidden:true,
+      },
+      {
+        path: 'team',
+        name: 'team',
+        component: team,
+        hidden: true,
+      },
+      {
+        path: 'CourseDevelopment',
+        name: 'CourseDevelopment',
+        component: CourseDevelopment,
+        hidden: true,
+      },
+      {
+        path: 'courseresource',
+        name:'courseresource',
+        component: courseresource,
+        hidden: true,
+      },
+      {
+        path: 'console',
+        name: 'console',
+        component: console,
+        hidden: true,
+      },
+      {
+        path: 'coursestudy',
+        name: 'coursestudy',
+        component: coursestudy,
+        hidden: true,
+      },
+      {
+        path: 'Team',
+        name: 'Team',
+        component: team,
+        hidden: true,
+        children:[
+          {
+            path:'team1',
+            name:'team1',
+            component:team1,
+            hidden:true
+          },
+          {
+            path: 'teacher1',
+            name: 'teacher1',
+            component: teacher1,
+            hidden: true,
+          }
+        ]
+      },
+      {
+        path:'course',
+        name: 'course',
+        component: introduce,
+        hidden: true,
+        children: [
+          {
+            path: 'description',
+            name:'description',
+            component: description,
+            hidden: true,
+          },
+          {
+            path: 'arrange',
+            name: 'arrange',
+            component: arrange,
+            hidden: true,
+          },
+          {
+            path: 'outline',
+            name: 'outline',
+            component: outline,
+            hidden: true,
+          },
+          {
+            path: 'notion',
+            name: 'notion',
+            component: notion,
+            hidden: true,
+          }
+        ]
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  // mode: 'history',
+  routes,
 })
 
 export default router
